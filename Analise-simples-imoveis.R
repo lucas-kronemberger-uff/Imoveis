98fb34bb92152ccd9429500cbbe3f95c0b0f6a4f
lucas-kronemberger-uff <lkronemberger@id.uff.br>
2026-04-02 22:10:11 -0300

    Delete Analise-simples-imoveis.R

--- Analise-simples-imoveis.R
+++
@@ -1,7 +0,0 @@
-#Script referente à atividade da disciplina "Prática Estatística"
-
-#Instalação de biblioteca para auxiliar na anáise de dados:
-library(tidyverse)
-
-#Leitura do banco de dados:
-dados = read.csv("imoveis.csv")
