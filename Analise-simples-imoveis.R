8687cb046aae6e47a04dc7b5eabca3c3fff472d5
lucas-kronemberger-uff <lkronemberger@id.uff.br>
2026-04-02 22:07:16 -0300

    Add files via upload
    Leitura do banco de dados

---
+++ Analise-simples-imoveis.R
@@ -0,0 +1,7 @@
+#Script referente à atividade da disciplina "Prática Estatística"
+
+#Instalação de biblioteca para auxiliar na anáise de dados:
+library(tidyverse)
+
+#Leitura do banco de dados:
+dados = read.csv("imoveis.csv")
