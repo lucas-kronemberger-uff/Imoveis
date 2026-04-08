4bf18a875a8478098aaa796e8449237344646088
lucas-kronemberger-uff <lkronemberger@id.uff.br>
2026-04-08 19:15:09 -0300

    Script esqueleto

---
+++ imoveis-com-branches.R
@@ -0,0 +1,11 @@
+# 1 - Leitura do banco de dados:
+
+dados = read.csv2("imoveis.csv")
+
+# 2 - Gráficos:
+#(Preencher na branch Gráficos)
+#Faça um histograma da metragem
+
+# 3 - Medidas:
+# (Preencher na branch Medidas)
+#Calcule média, medina e desvio-padrão de imposto anual
