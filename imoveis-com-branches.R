7659c07f1a5f93ed46c04264780dfa339223930a
lucas-kronemberger-uff <lkronemberger@id.uff.br>
2026-04-08 19:34:11 -0300

    Script com gráficos

--- imoveis-com-branches.R
+++ imoveis-com-branches.R
@@ -1,11 +1,14 @@
 # 1 - Leitura do banco de dados:
 
+
 dados = read.csv2("imoveis.csv")
 dados$metragem = as.numeric(dados$metragem)
 
+
 # 2 - Gráficos:
+
 hist(dados$metragem)
+
+
 # 3 - Medidas:
-media_imposto_anual = mean(dados$imposto_anual)
-desvio_padrao_imposto_anual = sd(dados$imposto_anual)
-mediana_imposto_anual = median(dados$imposto_anual)
+# (Preencher na branch Medidas)
