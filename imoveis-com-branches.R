0b28f13649fec6698b21369af35f0acbffdfdeef
lucas-kronemberger-uff <lkronemberger@id.uff.br>
2026-04-08 19:22:18 -0300

    Script com gráficos

--- imoveis-com-branches.R
+++ imoveis-com-branches.R
@@ -1,11 +1,9 @@
 # 1 - Leitura do banco de dados:
 
 dados = read.csv2("imoveis.csv")
-
+dados$metragem = as.numeric(dados$metragem)
 # 2 - Gráficos:
-#(Preencher na branch Gráficos)
-#Faça um histograma da metragem
-
+hist(dados$metragem)
 # 3 - Medidas:
 # (Preencher na branch Medidas)
 #Calcule média, medina e desvio-padrão de imposto anual
